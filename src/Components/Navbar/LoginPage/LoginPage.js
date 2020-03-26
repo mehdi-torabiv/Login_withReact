dba6cad2429f4d310fd863154f10cc95f5bf0ec3
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-03-27 00:21:18 +0430

    create login form

--- src/Components/Navbar/LoginPage/LoginPage.js
+++ src/Components/Navbar/LoginPage/LoginPage.js
@@ -2,8 +2,29 @@ import React from 'react';
 import "./LoginPage.css";
 const LoginPage = () => {
     return (
-        <div>
-
+        <div className="loginPage">
+            <h2 className="headerLogin">Login Page!</h2>
+            <div>
+                <form>
+                    <div className="subdiv">
+                        <p>Username:</p>
+                        <input className="diffrent"></input>
+                    </div>
+                    <div className="subdiv">
+                        <p>Password:</p>
+                        <input className="diffrent"></input>
+                    </div>
+                    <br></br>
+                    <div className="checkbox subdiv">
+                        <input type="checkbox"></input>
+                        <p>Are you ok with policy?</p>
+                    </div>
+                    <div className="SubandDel">
+                        <button>Login</button>
+                        <button>Close</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./Navbar.css";
 import "./LoginPage/LoginPage";
+import LoginPage from './LoginPage/LoginPage';
 class Navbar extends Component {
     constructor(props) {
         super(props);
@@ -11,25 +12,28 @@ class Navbar extends Component {
     }
     render() {
         return (
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Community</a>
-                    </li>
-                    <li>
-                        <a href="#">About us</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact Us</a>
-                    </li>
-                    <li className="">
-                        <a href="#">Login</a>
-                    </li>
-                </ul>
-            </nav>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Community</a>
+                        </li>
+                        <li>
+                            <a href="#">About us</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                        <li className="">
+                            <a href="#">Login</a>
+                        </li>
+                    </ul>
+                </nav>
+                <LoginPage />
+            </div>
         );
     }
 }
